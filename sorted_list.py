455c870295717e774eab8a6bbd95f639352365c5
Roshni Gupta <roshni.g@turing.com>
2025-03-17 19:26:35 +0530

    initalize min heap

--- sorted_list.py
+++ sorted_list.py
@@ -9,3 +9,9 @@ class ListNode:
 def mergeKLists(lists):
     if not lists or len(lists) == 0:
         return None
+    
+        min_heap = []
+    
+    for i, l in enumerate(lists):
+        if l:
+            heappush(min_heap, (l.val, i, l))  # Push value, index, and node
